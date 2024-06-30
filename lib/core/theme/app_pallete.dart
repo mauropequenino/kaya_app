c71d448962b4b23a753ebd5bc04f1cd029e69a07
Mauro Pequenino <mauroernesto5@gmail.com>
2024-06-30 21:39:47 +0200

    updated

--- lib/core/theme/app_pallete.dart
+++ lib/core/theme/app_pallete.dart
@@ -13,6 +13,7 @@ class Pallete {
   static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
   static const Color whiteColor = Colors.white;
   static const Color greyColor = Colors.grey;
+  static const Color blackColor = Colors.black;
   static const Color errorColor = Colors.redAccent;
   static const Color transparentColor = Colors.transparent;
 
